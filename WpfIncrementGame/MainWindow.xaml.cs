9d9f7c88ce39f763a5b38c6d9f963ce483bbfa56
MadsStasel <mads464@yahoo.com>
2021-01-18 20:46:45 +0100

    Increment game with incredible basic functionallity
    Start of a little game I'd like to throw some time at :)

---
+++ WpfIncrementGame/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfIncrementGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void PunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            FistStrengthText.Content = (Convert.ToInt32(FistStrengthText.Content) + 1);
+        }
+
+        private void SideKickButton_Click(object sender, RoutedEventArgs e)
+        {
+            LegStrengthText.Content = Convert.ToInt32(LegStrengthText.Content) + 1;
+        }
+
+        private void PushUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            CoreStrengthText.Content = Math.Round(Convert.ToDouble(CoreStrengthText.Content) + 0.01, 2);
+        }
+    }
+}
