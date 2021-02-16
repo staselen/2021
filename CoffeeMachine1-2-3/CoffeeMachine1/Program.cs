9f887a2586735bb8c9ab99925de309fd970c9966
staselen <mads464@yahoo.com>
2021-02-16 13:55:58 +0100

    Add files via upload

---
+++ CoffeeMachine1-2-3/CoffeeMachine1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System
+    ;
+
+namespace CoffeeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create machine
+            CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+
+            //Add Beans
+            for (int i = 0; i < 20; i++)
+            {
+                coffeeMachine.AddBeans();
+
+            }
+
+            //Add Water
+            for (int i = 0; i < 5; i++)
+            {
+                coffeeMachine.AddWater();
+            }
+
+            //Add filter
+            coffeeMachine.AddFilter();
+
+            //Turn on machine
+            coffeeMachine.Power = true;
+
+            //Print status
+            Console.WriteLine(coffeeMachine.Status()+"\n");
+
+            //Make a cup of coffe
+            Cup cup = coffeeMachine.Make("Coffee");
+
+
+            if(cup == null)
+            {
+                Console.WriteLine("Couldn't make coffee");
+            }
+            else
+            {
+                Console.WriteLine("Made a cup of "+cup.Name);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CoffeeMachine1-2-3/CoffeeMachine1/WaterContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeMachine
+{
+    class WaterContainer
+    {
+        public List<string> Contains { get; set; }
+        public WaterContainer()
+        {
+            Contains = new List<string>();
+        }
+    }
+}
