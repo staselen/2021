8c0332fd35a45e444a7ddd5891a21d3d6fd86e48
staselen <mads464@yahoo.com>
2021-02-17 10:55:27 +0100

    Add files via upload

---
+++ WpfTicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfTicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Tick += AI;
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Start();
+        }
+
+        private void AI(object sender, EventArgs e)
+        {
+
+        }
+
+
+        bool x = true;
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if ((string)button.Content == "")
+            {
+                if(x)
+                {
+                    button.Content = "X";
+                    x = false;
+                }
+                else
+                {
+                    button.Content = "O";
+                    x = true;
+
+                }
+
+            }
+
+        }
+    }
+
+
+}
