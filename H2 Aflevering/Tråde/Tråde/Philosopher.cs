3fc6043b4c4e35ad52a3474dbc4e7b5cf45539f0
staselen <mads464@yahoo.com>
2021-05-28 12:32:51 +0200

    Add files via upload

---
+++ H2 Aflevering/Tråde/Tråde/Philosopher.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tråde
+{
+    class Philosopher
+    {
+        public int Position { get; set; } //The position of the Philosopher on the table 
+        public Plate Plate { get; set; } //The plate assigned to the Philosopher
+
+        public Philosopher(int position)
+        {
+            Position = position; //Sets the position with the parameter
+            Plate = new Plate(); //New plate
+            Plate.Food = new List<string>(); //New list of food on plate
+        }
+
+        public void Dining(object forksConverted)
+        {
+            //Converts object back to fork array
+            object[] forks = (object[])forksConverted;
+
+            Random r = new Random();
+
+            //Run while there is food on the plate of the philosopher
+            while (Plate.Food.Count != 0)
+            {
+                //Philosopher is thinking
+                Console.WriteLine("Philospher[{0}] Is thinking...", Position + 1, Math.Min(Position, (Position + 1) % 5));
+                Thread.Sleep(r.Next(5000));
+
+                //Calculation explained:
+                //% is Modulus, which returns the remainder of the division of two numbers
+                //If you calculate the modulus of two of the same number it will return 0
+                //This means that we can use modulus here to ensure that one philosopher
+                //(The last one) Grabs the forks in a different order than the rest.
+                //Every philosopher aside from the last will first take the fork correspondeing to their position
+                //(Philo#0 starts with fork#0) but the last philosopher will also start with 0
+                //Therefore preventing the possibilty of a deadlock.
+                //First - Last
+                //0     - 1
+                //1     - 2
+                //2     - 3
+                //3     - 4
+                //0     - 4
+
+                //Philosopher picks up first fork
+                Monitor.Enter(forks[Math.Min(Position, (Position + 1) % 5)]);
+                try
+                {
+                    //Philosopher picks up second fork
+                    Console.WriteLine("Philospher[{0}] picked up fork[{1}]", Position + 1, Math.Min(Position, (Position + 1) % 5) + 1);
+                    Monitor.Enter(forks[Math.Max(Position, (Position + 1) % 5)]);
+                    try
+                    {
+                        Thread.Sleep(200);
+                        Console.WriteLine("Philospher[{0}] picked up fork[{1}]", Position + 1, Math.Max(Position, (Position + 1) % 5) + 1);
+                        Thread.Sleep(200);
+                        Console.WriteLine("Philospher[{0}] starts eating", Position + 1);
+                        Thread.Sleep(5000);
+                        //Philosopher has eaten
+                        Plate.Food.RemoveAt(0); //Removes the eaten piece of food from plate
+                        Console.WriteLine("Philospher[{0}] is done eating", Position + 1);
+
+                        Thread.Sleep(350);
+                        //Philosopher puts down second fork
+                        Console.WriteLine("Philospher[{0}] puts down fork[{1}]", Position + 1, Math.Max(Position, (Position + 1) % 5) + 1);
+                    }
+                    finally
+                    {
+                        Monitor.PulseAll(forks[Math.Max(Position, (Position + 1) % 5)]); //Notifies the lock(Fork) is relased
+                        Monitor.Exit(forks[Math.Max(Position, (Position + 1) % 5)]);     //Releases the lock(Fork)
+                    }
+                    Thread.Sleep(500);
+                    //Philosopher puts down fork first
+                    Console.WriteLine("Philospher[{0}] puts down fork[{1}]", Position + 1, Math.Min(Position, (Position + 1) % 5) + 1);
+                }
+                finally
+                {
+                    Monitor.PulseAll(forks[Math.Min(Position, (Position + 1) % 5)]); //Release the lock(Fork)
+                    Monitor.Exit(forks[Math.Min(Position, (Position + 1) % 5)]);  //Notifies the lock(Fork) is released
+
+                }
+
+            }
+            //Philosophers plate is empty
+            Console.WriteLine("Philospher[{0}] plate is empty", Position + 1);
+        }
+
+
+
+
+
+    }
+}
---
+++ H2 Aflevering/Tråde/Tråde/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace Tråde
+{
+    class Program
+    {
+        //There's a fork for every philosopher, but they need two to eat so they must share.
+        static void Main(string[] args)
+        {
+            //Craete array for 5 Philosophers
+            Philosopher[] philosophers = new Philosopher[5];
+            //Populate philosophers array
+            for (int i = 0; i < 5; i++)
+            {
+                philosophers[i] = new Philosopher(i);
+            }
+
+            //Add 5 Spaghetto to each philosophers plate
+            for (int i = 0; i < 5; i++)
+            {
+                //Spaghetto is the word for a Singular Spaghetti
+                for (int s = 0; s < 5; s++)
+                {
+                    philosophers[i].Plate.Food.Add("Spaghetto");
+                }
+            }
+
+
+            //Create object array of forks(locks)
+            object[] forks = new object[5] { new object(), new object(), new object(), new object(), new object()};
+            //Convert object array to object
+            //Paramaterized Thread Starts can only take objects
+            object forksConverted = forks; 
+            //Create array of 5 threads(I wanted a reference to join the threads)
+            Thread[] thread = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+                thread[i] = new Thread(new ParameterizedThreadStart(philosophers[i].Dining));
+                thread[i].Start(forksConverted); //Pass the converted fork array
+            }
+
+            //For every thread in the thread array join them
+            //Makes the main thread pause untill all threads are done running.
+            //Allows me to print in console when all the philosophers are done eating.
+            for (int i = 0; i < 5; i++)
+            {
+                thread[i].Join();
+            }
+
+            Console.WriteLine("All philosophers has finished eating");
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
